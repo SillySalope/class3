05a2ea1816ca4bcc6551b74ba39f677c93197a41
Mariano <mmoranve@gmail.com>
2018-02-13 00:53:06 -0500

    finally works
    missing loop, marker as icon, and legend

--- js/scripts.js
+++ js/scripts.js
@@ -1,114 +1,154 @@
+// 1. Map
 // Define position and zoom
-var centerMap =[-33.91790, 151.23463];
+var centerMap =[19.409703,-99.142739];
 var zoomMap= 12;
 // Define the map
-var map = L.map('my-map').setView(centerMap, zoomMap);
-
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+var map = L.map('gf-map').setView(centerMap, zoomMap);
+// Define the layer for the map
+var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'png'
 }).addTo(map);
 
+// 2. Script to integrate the homes and the markers into the map
 
-// var colors = [
-//   'blue',
-//   'red',
-// ]
+var gf = [
+  {
+    gf: 'MF',
+    r_we_cool: "Cool",
+    lat: 19.426914,
+    lon: -99.171562,
+    year: 2010
+  },
+  {
+    gf: 'MC',
+    r_we_cool: "Nah",
+    lat: 19.424443,
+    lon: -99.155340,
+    year: 2014
+  },
+];
 
-// map //
-
-// L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-// 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-// 	subdomains: 'abcd',
-// 	minZoom: 1,
-// 	maxZoom: 16,
-// 	ext: 'png'
-// }).addTo(map);
-
-
-// getPlaces((places) => {
+// how to add a single marker using L.marker()
+// var chrisPizza = pizzaData[0];
 //
-//   places.forEach((place) => {
+// L.marker([chrisPizza.lat, chrisPizza.lon]).addTo(map)
+//     .bindPopup(chrisPizza.name + ' likes to eat at ' +  chrisPizza.pizzaShop);
+
+
+// create an empty markers array that we can fill with markers
+var markersArray = [];
+
+// how to add a marker for each object in the array
+
+gf.forEach(function(home) {
+  var latLon = [home.lat, home.lon];
+
+  var r_we_coolColor = '#FFF';
+
+  if (home.r_we_cool === 'Cool') r_we_coolColor = 'blue';
+  if (home.r_we_cool === 'Nah') r_we_coolColor = 'red';
+
+  var MarkerOptions = {
+    radius: 6,
+    opacity: 1,
+    fillColor: r_we_coolColor,
+    fillOpacity: 0.9,
+    color: '#FFF',
+    weight: 2,
+  };
+
+  var marker = L.circleMarker(latLon, MarkerOptions)
+      .bindPopup('Are we cool with' + ' ' + home.gf + '?' + '<br><b>' + home.r_we_cool + '</b>'+' '+'since'+ ' ' + home.year, {offset: [0, -6]})
+      .addTo(map)
+  // add the marker to the markersArray
+  markersArray.push(marker);
+});
+
+// //A function to call the homes from the spreadsheet
+// getPlaces((homes) => {
+// //for each...
+//   homes.forEach((home) => {
 //
-//     const latLon = [place.lat, place.lon];
+//     const latLon = [home.lat, home.lon];
 //
-//     const typePalette = {
+//     const r_we_coolPalette = {
 //       Cool: 'red',
 //       Nah: 'blue',
 //     };
 //
-//     const placeColor = typePalette[place.type];
+//     const homeColor = r_we_coolPalette[home.r_we_cool];
 //
 //     const circleOptions = {
 //       stroke: false,
-//       radius: place.rate,
+//       // radius: home.rate,
 //       fillOpacity: 0.8,
-//       fillColor: placeColor,
-//       width: 0
-//     }
+//       fillColor: blue, //how to change it to blue or red depending on the coolness?
+//       width: 1
+//     });
 //
 //     L.circleMarker(latLon, circleOptions).addTo(map)
-//         .bindPopup('<b>' + place.link  + ' (' +place.year+') '+ '</b><br>' + place.location );
+//         .bindPopup('Are we cool with' + home.gf + '?' + '<b>'home.r_we_cool'</b><br>'+' '+'since'+ home.year);
 //   });
 // });
 //
-// var legend = L.control({position: 'bottomright'});
-//
-// legend.onAdd = function (map) {
-//
-//   var div = L.DomUtil.create('div', 'info legend');
-//
-//     categories = ['Nah','Cool'];
-//     colors = ['blue','red'],
-//
-//     for (var i = 0; i < colors.length) {
-//       div.innerHTML +=
-//           '<i class="circle" style="background:' + getColor(colors[i]) + '"></i> ' +
-//            (colors[i] ? colors[i] + '<br>' : '+');
-//     }
-//
-//     return div;
-//   }
-// legend.addTo(map);
+// //Adding legend
+// // var legend = L.control({position: 'bottomright'});
+// //
+// // legend.onAdd = function (map) {
+// //
+// //   var div = L.DomUtil.create('div', 'info legend');
+// //
+// //     categories = ['Nah','Cool'];
+// //     colors = ['blue','red'],
+// //
+// //     for (var i = 0; i < colors.length) {
+// //       div.innerHTML +=
+// //           '<i class="circle" style="background:' + getColor(colors[i]) + '"></i> ' +
+// //            (colors[i] ? colors[i] + '<br>' : '+');
+// //     }
+// //
+// //     return div;
+// //   }
+// // legend.addTo(map);
 //
 // // LEGEND TUTORIAL: http://ghost.mixedbredie.net/legendary-leaflet-legends/
 // // http://leafletjs.com/examples/choropleth/
 //
-// function getColor(d) {
-//   switch (d) {
-//     case 'Action': return 'gold';
-//     case 'Comedy': return 'springgreen';
-//     case 'Crime': return 'dodgerblue';
-//     case 'Drama': return 'plum';
-//     case 'Fantasy': return 'seagreen';
-//     case 'Romance': return 'fuchsia';
-//   }
-// };
-//
-// var legend = L.control({position: 'bottomleft'});
-//
-// legend.onAdd = function (map) {
-//   var div = L.DomUtil.create('div', 'info legend');
-//     films = ['Action','Comedy','Crime','Drama','Fantasy','Romance'];
+// // function getColor(d) {
+// //   switch (d) {
+// //     case 'Cool': return 'blue';
+// //     case 'Nah': return 'red';
+// //   }
+// // };
+// //
+// // // var legend = L.control({position: 'bottomleft'});
+// //
+// // legend.onAdd = function (map) {
+// //   var div = L.DomUtil.create('div', 'info legend');
+// //     are we cool? = ['Cool','Nah'];
 //
 //   // loop through the categories values and generate a label with a circle for each value
 //
-//   for (var i = 0; i < films.length; i++) {
-//
-//     div.innerHTML +=   '<i class="circle" style="background:' + getColor(films[i]) + '"></i> ' + (films[i] ? films[i] + '<br>' : '+');
-//   }
-// 	return div;
-// };
-// legend.addTo(map);
+// //   for (var i = 0; i < films.length; i++) {
+// //
+// //     div.innerHTML +=   '<i class="circle" style="background:' + getColor(films[i]) + '"></i> ' + (films[i] ? films[i] + '<br>' : '+');
+// //   }
+// // 	return div;
+// // };
+// // legend.addTo(map);
 //
 // // in spreadsheet: File --> Publish to web --> copy link
 //
 // function getPlaces(callback) {
 //   $.ajax({
-//     //url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTtDhxGKMcnLTHkSHCURW5HACFOSPSOOGSTpEY3C7PH8Rk1Nq8ZFVvhihfVEQmGB25iyQ3e9B3ADLgY/pub?gid=0&single=true&output=csv",
-//     url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRtWdBOaRWUXNgzNbSTIJ3uz1NGe7Ct2Rq6kGE9BCQEvLYi9k0uumCa8Z8VJfPsbJrM6FT9j-6QUHS5/pub?output=csv",
+//     url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRn2qchpmAlRGtftH5ragZ3GNYDGHqmFp-4LRZO7Vfbpt1OFcP3DWVUj_xG8NgkmAUOJqqlpIqjt5hf/pub?output=csv",
 //     type: "GET"
 //   }).done((csv) => {
-//     const places = Papa.parse(csv, {header: true}).data;
-//     callback(places);
+//     const homes = Papa.parse(csv, {header: true}).data;
+//     callback(homes);
 //   });
 // }
