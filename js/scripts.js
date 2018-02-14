57125512faee2f767682a57176a0236403fc1935
Mariano <mmoranve@gmail.com>
2018-02-14 12:29:59 -0500

    green marker
    cleaned up code, added a marker for my pops

--- js/scripts.js
+++ js/scripts.js
@@ -13,66 +13,6 @@ var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/w
 	ext: 'png'
 }).addTo(map);
 
-// 2. Script to integrate the homes and the markers into the map
-
-// var gfData = [
-//   {
-//     gf: 'MF',
-//     r_we_cool: "Cool",
-//     lat: 19.426914,
-//     lon: -99.171562,
-//     year: 2010
-//   },
-//   {
-//     gf: 'MC',
-//     r_we_cool: "Nah",
-//     lat: 19.424443,
-//     lon: -99.155340,
-//     year: 2014
-//   },
-// 	{
-// 		gf: 'AL',
-// 		r_we_cool: "Nah",
-// 		lat: 19.389992,
-// 		lon: -99.113706,
-// 		year: 2006
-// 	},
-// 	{
-// 		gf: 'DA',
-// 		r_we_cool: "Nah",
-// 		lat: 19.3608,
-// 		lon: -99.132578,
-// 		year: 2004
-// 	},
-// 	{
-// 		gf: 'B',
-// 		r_we_cool: "Nah",
-// 		lat: 19.351305,
-// 		lon: -99.119296,
-// 		year: 2012
-// 	},
-// 	{
-// 		gf: 'C',
-// 		r_we_cool: "Cool",
-// 		lat: 19.350442,
-// 		lon: -99.205116,
-// 		year: 2016
-// 	},
-// 	{
-// 		gf: 'A',
-// 		r_we_cool: "Nah",
-// 		lat: 19.351735,
-// 		lon: -99.122847,
-// 		year: 2008
-// 	},
-// ];
-
-// how to add a single marker using L.marker()
-// var chrisPizza = pizzaData[0];
-//
-// L.marker([chrisPizza.lat, chrisPizza.lon]).addTo(map)
-//     .bindPopup(chrisPizza.name + ' likes to eat at ' +  chrisPizza.pizzaShop);
-
 var fheartIcon = L.icon({
 		iconUrl: 'img/fheart.png',
 		// iconSize:     [38, 95], // size of the icon
@@ -93,29 +33,6 @@ var bheartIcon = L.icon({
 // create an empty markers array that we can fill with markers
 // var markersArray = [];
 
-//1. OPERATING CODE
-
-// gfData.forEach(function(home) {
-//   var latLon = [home.lat, home.lon];
-//
-//   var r_we_coolColor = '#FFF';
-//   	if (home.r_we_cool === 'Cool') r_we_coolColor = 'blue';
-//   	if (home.r_we_cool === 'Nah') r_we_coolColor = 'red';
-//
-//   var MarkerOptions = {
-//     radius: 6,
-//     opacity: 1,
-//     fillColor: r_we_coolColor,
-//     fillOpacity: 0.9,
-//     color: '#FFF',
-//     weight: 2,
-//   };
-//
-// 		L.circleMarker(latLon, MarkerOptions)
-// 		.bindPopup('Are we cool with' + ' ' + home.gf + '?' + '<br><b>' + home.r_we_cool + '</b>'+' '+'since'+ ' ' + home.year, {offset: [0, -6]})
-// 		.addTo(map);
-// 	});
-
 // 2. OPERATING CODE MODIFIED FOR GETPLACES FUNCTION
 getPlaces((places) => {
 	places.forEach(function(home) {
@@ -124,6 +41,7 @@ getPlaces((places) => {
 	  var r_we_coolColor = '#FFF';
 	  	if (home.r_we_cool === 'Cool') r_we_coolColor = 'blue';
 	  	if (home.r_we_cool === 'Nah') r_we_coolColor = 'red';
+			if (home.r_we_cool === 'Of Course') r_we_coolColor = 'green';
 
 	  const MarkerOptions = {
 	    radius: 10,
@@ -174,80 +92,6 @@ getPlaces((places) => {
 //   markersArray.push(marker);
 // });
 
-// Adapting RIGEL CODE
-// getPlaces((places) => {
-//
-//   places.forEach((place) => {
-//
-//     const latLon = [place.lat, place.lon];
-//
-//     const r_we_coolPalette = {
-// 			Cool: 'red',
-// 			Nah: 'blue',
-//     };
-//
-//     const placeColor = typePalette[place.r_we_cool];
-//
-//     const circleOptions = {
-//       stroke: false,
-//       fillOpacity: 0.8,
-//       fillColor: placeColor,
-//       width: 0
-//     }
-//
-//     L.circleMarker(latLon, circleOptions).addTo(map)
-//         .bindPopup('Are we cool with' + home.gf + '?' + '<b>'home.r_we_cool'</b><br>'+' '+'since'+ home.year);
-//   });
-// });
-
-// //Adding legend
-// // var legend = L.control({position: 'bottomright'});
-// //
-// // legend.onAdd = function (map) {
-// //
-// //   var div = L.DomUtil.create('div', 'info legend');
-// //
-// //     categories = ['Nah','Cool'];
-// //     colors = ['blue','red'],
-// //
-// //     for (var i = 0; i < colors.length) {
-// //       div.innerHTML +=
-// //           '<i class="circle" style="background:' + getColor(colors[i]) + '"></i> ' +
-// //            (colors[i] ? colors[i] + '<br>' : '+');
-// //     }
-// //
-// //     return div;
-// //   }
-// // legend.addTo(map);
-//
-// // LEGEND TUTORIAL: http://ghost.mixedbredie.net/legendary-leaflet-legends/
-// // http://leafletjs.com/examples/choropleth/
-//
-// // function getColor(d) {
-// //   switch (d) {
-// //     case 'Cool': return 'blue';
-// //     case 'Nah': return 'red';
-// //   }
-// // };
-// //
-// // // var legend = L.control({position: 'bottomleft'});
-// //
-// // legend.onAdd = function (map) {
-// //   var div = L.DomUtil.create('div', 'info legend');
-// //     are we cool? = ['Cool','Nah'];
-//
-//   // loop through the categories values and generate a label with a circle for each value
-//
-// //   for (var i = 0; i < films.length; i++) {
-// //
-// //     div.innerHTML +=   '<i class="circle" style="background:' + getColor(films[i]) + '"></i> ' + (films[i] ? films[i] + '<br>' : '+');
-// //   }
-// // 	return div;
-// // };
-// // legend.addTo(map);
-//
-// // in spreadsheet: File --> Publish to web --> copy link
-//
 function getPlaces(callback) {
   $.ajax({
     url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRn2qchpmAlRGtftH5ragZ3GNYDGHqmFp-4LRZO7Vfbpt1OFcP3DWVUj_xG8NgkmAUOJqqlpIqjt5hf/pub?output=csv",
